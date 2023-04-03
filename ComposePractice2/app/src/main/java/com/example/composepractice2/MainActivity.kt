70c629e94e16e9f18aec003d905724d5e5b804bb
brightalot <gamja9151@hanyang.ac.kr>
2023-04-04 02:27:55 +0900

    onCreate() modify, TaskManagerWithText() create

--- ComposePractice2/app/src/main/java/com/example/composepractice2/MainActivity.kt
+++ ComposePractice2/app/src/main/java/com/example/composepractice2/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.example.composepractice2
 
+import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composepractice2.ui.theme.ComposePractice2Theme
-
+import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.intellij.lang.annotations.JdkConstants.TitledBorderJustification
+import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TaskManagerWithImage("Android")
                 }
             }
         }
@@ -31,8 +42,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TaskManagerWithText(body1: String, body2: String, modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = body1,
+            fontSize = 24.dp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = body2,
+            fontSize = 16.sp
+        )
+
+
+    }
+
 }
 
 @Preview(showBackground = true)
