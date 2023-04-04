91434f1a8c017ac12a0b015a0962765407e8607b
brightalot <gamja9151@hanyang.ac.kr>
2023-04-05 01:39:13 +0900

    submit ComposePractice3

--- ComposePractice3/app/src/main/java/com/example/composepractice3/MainActivity.kt
+++ ComposePractice3/app/src/main/java/com/example/composepractice3/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.composepractice3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composepractice3.ui.theme.ComposePractice3Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +25,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposePractice3Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.background) {
+                    QuadrantGenerator()
                 }
             }
         }
@@ -30,14 +34,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun QuadrantGenerator() {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier.weight(1f)
+        ) {
+            QudrantText(title = stringResource(R.string.second_quadrant_title),
+                body = stringResource(R.string.second_quadrant_body),
+                color = androidx.compose.ui.graphics.Color.Green,
+                modifier = Modifier.weight(1f))
+            QudrantText(title = stringResource(R.string.first_quadrant_title),
+                body = stringResource(R.string.first_quadrant_body),
+                color = androidx.compose.ui.graphics.Color.Yellow,
+                modifier = Modifier.weight(1f))
+
+        }
+        Row(
+            modifier = Modifier.weight(1f)
+        ) {
+            QudrantText(title = stringResource(R.string.third_quadrant_title),
+                body = stringResource(R.string.third_quadrant_body),
+                color = androidx.compose.ui.graphics.Color.Cyan,
+                modifier = Modifier.weight(1f))
+            QudrantText(title = stringResource(R.string.fourth_quadrant_title),
+                body = stringResource(R.string.fourth_quadrant_body),
+                color = androidx.compose.ui.graphics.Color.LightGray,
+                modifier = Modifier.weight(1f))
+        }
+    }
 }
+
+@Composable
+private fun QudrantText (title: String, body: String, color: Color, modifier: Modifier = Modifier) {
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+            .background(color)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = body,
+            textAlign = TextAlign.Justify
+        )
+
+
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposePractice3Theme {
-        Greeting("Android")
+        QuadrantGenerator()
     }
 }
