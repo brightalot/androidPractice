894c85ba3b6f54196dfe9d965b2bbcb6696ad001
brightalot <gamja9151@hanyang.ac.kr>
2023-04-14 23:23:58 +0900

    import com.example.secondactivitypractice.SecondActivity 삭제

--- SecondActivityPractice/app/src/main/java/com/example/secondactivitypractice/MainActivity.kt
+++ SecondActivityPractice/app/src/main/java/com/example/secondactivitypractice/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.secondactivitypractice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.secondactivitypractice.SecondActivity
+
 import com.example.secondactivitypractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
