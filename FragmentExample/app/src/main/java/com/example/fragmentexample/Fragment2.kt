14997ed2c75ea9d7388ca3513848f3cfc62f1dd1
brightalot <gamja9151@hanyang.ac.kr>
2023-04-15 04:21:23 +0900

    FragmentExample create

---
+++ FragmentExample/app/src/main/java/com/example/fragmentexample/Fragment2.kt
@@ -0,0 +1,7 @@
+package com.example.fragmentexample
+
+import androidx.fragment.app.Fragment
+
+class Fragment2 : Fragment() {
+
+}
---
+++ FragmentExample/app/src/main/java/com/example/fragmentexample/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.fragmentexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.fragment.app.Fragment
+import com.example.fragmentexample.databinding.ActivityMainBinding
+
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var fragment1: Fragment1
+    private lateinit var fragment2: Fragment2
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fragment1 = Fragment1()
+        fragment2 = Fragment2()
+
+        binding.btn1.setOnClickListener {
+            showFragment(fragment1)
+        }
+
+        binding.btn2.setOnClickListener {
+            showFragment(fragment2)
+        }
+    }
+    private fun showFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame, fragment)
+            .commit()
+    }
+}
