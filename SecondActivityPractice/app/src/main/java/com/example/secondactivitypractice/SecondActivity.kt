acbf63f0c1a97350dd11272dd1bcf45a4622dc1a
brightalot <gamja9151@hanyang.ac.kr>
2023-04-14 23:31:15 +0900

    SecondActivity 화면에서 다시 MainActivity로 돌아가는 버튼 추가

--- SecondActivityPractice/app/src/main/java/com/example/secondactivitypractice/SecondActivity.kt
+++ SecondActivityPractice/app/src/main/java/com/example/secondactivitypractice/SecondActivity.kt
@@ -1,5 +1,6 @@
 package com.example.secondactivitypractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.secondactivitypractice.databinding.ActivitySecondBinding
@@ -14,8 +15,14 @@ class SecondActivity : AppCompatActivity() {
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // MainActivity에서 전달한 메시지를 받아와서 TextView에 표시
-        val message = intent.getStringExtra("MESSAGE")
-        binding.textView.text = message
+//        // MainActivity에서 전달한 메시지를 받아와서 TextView에 표시
+//        val message = intent.getStringExtra("MESSAGE")
+//        binding.textView.text = message
+
+        binding.secondButton.setOnClickListener {
+            // 버튼을 눌렀을 때 실행될 코드
+            val second_intent = Intent(this@SecondActivity, MainActivity::class.java)
+            startActivity(second_intent)
+        }
     }
 }
