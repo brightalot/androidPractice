7e796fb15475c01c059398143d168becf343167e
brightalot <gamja9151@hanyang.ac.kr>
2023-04-20 17:22:31 +0700

    registerForActivityResult() 를 이용해 secondActivity 에서 전달받은 backmessage 를 tv_main 의 text로 바꾸도록 설정함

--- SecondActivityPractice/app/src/main/java/com/example/secondactivitypractice/MainActivity.kt
+++ SecondActivityPractice/app/src/main/java/com/example/secondactivitypractice/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.secondactivitypractice
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 
 import com.example.secondactivitypractice.databinding.ActivityMainBinding
 
@@ -10,21 +12,29 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    val secondActivityLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            val message = data?.getStringExtra("message")
+            binding.tvMain.text = message
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.editTextButton.setOnClickListener {
+        binding.etGo.setOnClickListener {
             //메세지 변수로 넣기
             val message = binding.sampleEdittext.text.toString()
             //경로 잡기
             val intent = Intent(this@MainActivity, SecondActivity::class.java)
-
             //메시지를 putExtra에 넣기
             intent.putExtra("message", message)
-            startActivity(intent)
+            secondActivityLauncher.launch(intent)
 
         }
 
--- SecondActivityPractice/app/src/main/java/com/example/secondactivitypractice/SecondActivity.kt
+++ SecondActivityPractice/app/src/main/java/com/example/secondactivitypractice/SecondActivity.kt
@@ -1,30 +1,36 @@
 package com.example.secondactivitypractice
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.secondactivitypractice.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySecondBinding
-
+    private lateinit var secondBinding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivitySecondBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        secondBinding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(secondBinding.root)
 
         // MainActivity에서 전달한 메시지를 받아와서 TextView에 표시
         val message = intent.getStringExtra("message")
-        binding.userText.text = message
+        secondBinding.tvGivenMessage.text = message
 
-        binding.secondButton.setOnClickListener {
+        secondBinding.btnSecond.setOnClickListener {
             // 버튼을 눌렀을 때 실행될 코드
-
+            val backmessage = secondBinding.etBack.text.toString()
             val second_intent = Intent(this@SecondActivity, MainActivity::class.java)
-            startActivity(second_intent)
+                .putExtra("message", backmessage)
+            setResult(Activity.RESULT_OK, second_intent)
             finish()
         }
     }
+
+
 }
