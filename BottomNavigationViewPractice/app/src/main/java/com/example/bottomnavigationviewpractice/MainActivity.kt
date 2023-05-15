2080b80dcc6fba8876abc1e6e06f18563c4bd3e6
brightalot <gamja9151@hanyang.ac.kr>
2023-05-15 16:14:35 +0900

    3개의 아이템을 가진 BottomNavigationView 구현

--- BottomNavigationViewPractice/app/src/main/java/com/example/bottomnavigationviewpractice/MainActivity.kt
+++ BottomNavigationViewPractice/app/src/main/java/com/example/bottomnavigationviewpractice/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.bottomnavigationviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bottomnavigationviewpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.containerFragment.id, HomeFragment())
+            .commitAllowingStateLoss()
+
+        binding.bottomNavigationView.run {
+            setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.menu_home -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(binding.containerFragment.id, HomeFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_setting -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(binding.containerFragment.id, SettingFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_mypage -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(binding.containerFragment.id, MypageFragment())
+                            .commitAllowingStateLoss()
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.menu_home
+        }
     }
 }
