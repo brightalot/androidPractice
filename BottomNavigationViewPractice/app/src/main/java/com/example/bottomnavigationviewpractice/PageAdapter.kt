b63318d11ea0f1b9afd8541b66df048bbae90eef
brightalot <gamja9151@hanyang.ac.kr>
2023-05-26 23:00:53 +0900

    Update PageAdapter.kt

--- BottomNavigationViewPractice/app/src/main/java/com/example/bottomnavigationviewpractice/PageAdapter.kt
+++ BottomNavigationViewPractice/app/src/main/java/com/example/bottomnavigationviewpractice/PageAdapter.kt
@@ -1,9 +1,6 @@
 package com.example.bottomnavigationviewpractice
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class PageAdapter(fragment: Fragment): FragmentStateAdapter(fragment) {
