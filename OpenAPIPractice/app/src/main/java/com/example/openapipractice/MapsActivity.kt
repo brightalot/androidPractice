fde38cce5f263eb7409d261d843073ea3d9685b1
brightalot <gamja9151@hanyang.ac.kr>
2023-06-12 04:32:06 +0900

    init

--- OpenAPIPractice/app/src/main/java/com/example/openapipractice/MapsActivity.kt
+++ OpenAPIPractice/app/src/main/java/com/example/openapipractice/MapsActivity.kt
@@ -1,15 +1,23 @@
 package com.example.openapipractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.openapipractice.data.Library
+import com.example.openapipractice.data.SeoulPublicLibraryInfo
+import com.example.openapipractice.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.openapipractice.databinding.ActivityMapsBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     //6e6b5a706b627269313134576e4d5157
@@ -28,21 +36,53 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        loadLibrary()
+
+//        // Add a marker in Sydney and move the camera
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+
+    fun loadLibrary() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(SeoulOpenApi.DOMAIN)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(SeoulOpenService::class.java)
+
+        service.getLibraries(SeoulOpenApi.API_KEY, 200)
+            .enqueue(object : Callback<Library> {
+                override fun onFailure(call: Call<Library>, t: Throwable) {
+                    Toast.makeText(this@MapsActivity, "데이터를 가져올 수 없습니다.", Toast.LENGTH_LONG).show()
+                }
+                override fun onResponse(call: Call<Library>, response: Response<Library>) {
+                    val result = response.body()
+                    showLibrary(result)
+                }
+            })
+    }
+    fun showLibrary(result:Library?) {
+
+        result.let{
+            val latLngbounds = LatLngBounds.builder()
+                for(library in it!!.SeoulPublicLibraryInfo.row) {
+                    val position = LatLng(library.XCNTS.toDouble(), library.YDNTS.toDouble())
+                    val marker = MarkerOptions().position(position).title(library.LBRRY_NAME)
+                    mMap.addMarker(marker)
+
+                    latLngbounds.include(position)
+                }
+            val bounds = latLngbounds.build()
+            val padding = 0
+
+            val camera = CameraUpdateFactory.newLatLngBounds(bounds, padding)
+            mMap.moveCamera(camera)
+        }
+    }
+
 }
